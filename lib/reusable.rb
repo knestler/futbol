1865784ef10033e0ddf9b1359ba188166784fd04
Krisetn Nestler <kristen.nestler@gmail.com>
2022-08-03 12:34:07 -0400

    clean up reusable module

--- lib/reusable.rb
+++ lib/reusable.rb
@@ -1,9 +1,6 @@
 module Reusables
   
-  
-  def team_win_percent_by_season # Provides hash of seasons with array of hashes for team id
-                                  #and win percentage for season - Helper for Issue #27
-    # Example hash: {20132014=> [{:team_id=>1, :win_perc=>50.0}, {:team_id=>4, :win_perc=>40.0}, {:team_id=>26, :win_perc=>100.0}
+  def team_win_percent_by_season
     team_win_percent = Hash.new {0}
     team_by_id.each do |id , team|
         season_win_percentage(id).each do |season, win|
@@ -19,8 +16,7 @@ module Reusables
     team_win_percent
   end
 
- def season_win_percentage(team_id) # Percentage of won games per season by team - helper method for issue #18
-    # {20132014=>50.0, 20142015=>14.3, 20162017=>50.0}
+  def season_win_percentage(team_id)
     win_percentage = Hash.new
     number_team_games_per_season(team_id).each do |game_season, game_count|
       number_team_wins_per_season(team_id).each do |wins_season, win_count|
@@ -37,8 +33,7 @@ module Reusables
     win_percentage
   end
 
-    def number_team_games_per_season(team_id) # Count of number of games a team played each season - helper method for issue #18
-    # {20122013=>1, 20132014=>2, 20142015=>7, 20162017=>4}
+  def number_team_games_per_season(team_id)
     team_games_by_season = Hash.new{0}
     games_by_season.each do |season, games|
       games_by_team(team_id).each do |result_data|
@@ -52,8 +47,7 @@ module Reusables
     team_games_by_season
   end
 
-    def games_by_team(team_id) # List of every game a team played - helper method for issue #18
-    # [[2013020252, 16], [2013020987, 16], [2014020903, 16], [2012020574, 16], [2014030161, 16],
+  def games_by_team(team_id)
     games = []
     @game_teams.each do |row|
       games <<[row[:game_id], team_id] if (row[:team_id] == team_id)
@@ -61,9 +55,7 @@ module Reusables
     games
   end
 
-
-  def number_team_wins_per_season(team_id) # Count of number of games a team won each season -helper method for issue #18
-    # {20132014=>1, 20142015=>1, 20162017=>2}
+  def number_team_wins_per_season(team_id)
     wins_by_season = Hash.new{0}
     games_by_season.each do |season, games|
       wins_by_team(team_id).each do |result_data|
@@ -78,12 +70,9 @@ module Reusables
     wins_by_season
   end
 
-    def wins_by_team(team_id) # List of every game that was a win for a team - helper method for issue #18
+  def wins_by_team(team_id)
     result_by_team = @game_teams.values_at(:game_id, :team_id, :result).find_all do |game|
       game[1] == team_id && game[2] == "WIN"
     end
   end
-
-
-
 end
