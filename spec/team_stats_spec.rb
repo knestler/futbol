0efeae32bd4b26902d4c63a12c055bbb4844c8ec
Stephen Fabian <fabianstephen@gmail.com>
2022-08-03 10:29:28 -0600

    added spec file for team_stats helper methods

---
+++ spec/team_stats_spec.rb
@@ -0,0 +1,15 @@
+require_relative 'spec_helper.rb'
+require 'csv'
+require './lib/team_stats'
+require './lib/csv_loader'
+require './lib/details_loader'
+
+describe Team do
+
+
+it 'test' do
+ 
+ 
+end
+
+end
