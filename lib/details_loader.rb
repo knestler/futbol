2c8746b2cc5cf727c2a448d206b71ea536552f1a
Aleisha Mork <aleisha.mork@gmail.com>
2022-08-03 01:18:28 -0600

    added test spec for details loader and cleaned up details loader class

--- lib/details_loader.rb
+++ lib/details_loader.rb
@@ -10,16 +10,13 @@ class DetailsLoader < CsvLoader
     @team_by_id = @teams.values_at(:team_id, :teamname).uniq.to_h
   end
 
-  def add_coach_by_team_id #need test
+  def add_coach_by_team_id
     coaches = Hash.new {|h, k| h[k] = {}}
     @games_by_season.each do |season, games|
       @game_teams.each do |row|
         if games.include?(row[:game_id])
-          if !coaches[row[:team_id]].keys.include?(season)
-            coaches[row[:team_id]][season] = [row[:head_coach]]
-          elsif !coaches[row[:team_id]][season].include?(row[:head_coach])
-            coaches[row[:team_id]][season] << row[:head_coach]
-          end
+          coaches[row[:team_id]][season] = [row[:head_coach]] if !coaches[row[:team_id]].keys.include?(season)
+          coaches[row[:team_id]][season] << row[:head_coach] if !coaches[row[:team_id]][season].include?(row[:head_coach])
         end
       end
     end
